6187b6acdc9b29d1a8101a13aacb6fdbce9df761
skyymar69-rgb <skyymar69@gmail.com>
2026-03-11 08:42:43 +0100

    Create HomePage component with Header, Hero, and Services components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <Services />
+        </div>
+    );
+};
+
+export default HomePage;
