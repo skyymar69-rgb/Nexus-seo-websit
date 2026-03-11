02a5b6bb6fd128ac50142e764b0c2558af00813b
skyymar69-rgb <skyymar69@gmail.com>
2026-03-11 08:24:41 +0100

    Create src/main.jsx file with React entry point

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/globals.css';
+import './styles/animations.css';
+
+ReactDOM.createRoot(document.getElementById('app')).render(<React.StrictMode><App /></React.StrictMode>,)
